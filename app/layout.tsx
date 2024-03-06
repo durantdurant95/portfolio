3d69f27da0549bb5f3031dcb4717552aa4bdf107
Alejandro Pérez Durán <aperez@datamart.cl>
2024-03-06 00:24:13 -0500

    Install tailwind prettier plugin

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,18 +18,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${comfortaa.className} flex h-screen flex-col p-4`}>
-        <nav className="flex justify-center items-center gap-4">
-          <div className="grow h-[1px] bg-black " />
+        <nav className="flex items-center justify-center gap-4">
+          <div className="h-[1px] grow bg-black " />
           <Link href="/">Durant</Link>
-          <div className="grow h-[1px] bg-black" />
+          <div className="h-[1px] grow bg-black" />
         </nav>
         {children}
-        <footer className="flex justify-center items-center gap-4">
+        <footer className="flex items-center justify-center gap-4">
           {/* //TODO: Add real links to social media */}
           <Link href="/work">Work</Link>
           <Link href="/work">Work</Link>
           <Link href="/work">Work</Link>
-          <div className="grow h-[1px] bg-black" />
+          <div className="h-[1px] grow bg-black" />
         </footer>
       </body>
     </html>
