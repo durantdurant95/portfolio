327e59d33ced8f87837cc28cdab049356fa4bf36
Alejandro Pérez Durán <aperez@datamart.cl>
2024-04-12 14:10:07 -0400

    Update Contact page with new links and download functionality

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,5 @@
-import { FiMail, FiPhone } from "react-icons/fi";
+import Link from "next/link";
+import { FiFileText, FiMail, FiPhone } from "react-icons/fi";
 import { notoSerif } from "../fonts";
 
 type Props = {};
@@ -16,20 +17,28 @@ const Contact = (props: Props) => {
           intuitive and user-friendly. Let&apos;s create a website that your
           visitors will love using! Get in touch today.
         </p>
-        <a
+        <Link
+          href="/Alejandro_Resume.pdf"
+          download
+          className="relative flex h-8 w-52 items-center gap-2 p-1 text-lg font-semibold transition duration-300 ease-in-out before:absolute before:bottom-1 before:left-0 before:-z-10 before:h-[6px] before:w-full before:bg-celtic-300 hover:scale-[1.02] before:hover:h-7 before:hover:transform before:hover:duration-300"
+        >
+          <FiFileText className="h-5 w-5 pl-1" />
+          Download my CV
+        </Link>
+        <Link
           href="mailto:apduran95@gmail.com"
           className="relative flex h-8 w-64 items-center gap-2 p-1 text-lg font-semibold transition duration-300 ease-in-out before:absolute before:bottom-1 before:left-0 before:-z-10 before:h-[6px] before:w-full before:bg-celtic-300 hover:scale-[1.02] before:hover:h-7 before:hover:transform before:hover:duration-300"
         >
           <FiMail className="h-5 w-5 pl-1" />
           apduran95@gmail.com
-        </a>
-        <a
+        </Link>
+        <Link
           href="tel:+5358184065"
           className="relative flex h-8 w-44 items-center gap-2 p-1 text-lg font-semibold transition duration-300 ease-in-out before:absolute before:bottom-1 before:left-0 before:-z-10 before:h-[6px] before:w-full before:bg-celtic-300 hover:scale-[1.02] before:hover:h-7 before:hover:transform before:hover:duration-300"
         >
           <FiPhone className="h-5 w-5 pl-1" />
           +53 58 18 40 65
-        </a>
+        </Link>
       </section>
     </main>
   );
